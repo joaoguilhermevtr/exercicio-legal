136f6a96b71327589451c3058c036ebd61d9c913
unknown <33614@ETECAF.LOCAL>
2019-03-22 17:23:13 -0300

    num sei

---
+++ Assets/banana.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class banana : MonoBehaviour {
+
+    public bool crt;
+    public int lvlpoke=63;
+    public int atkpoke = 50;
+    public int poderpoke = 20;
+    public int mod = 1; 
+
+    void Start () {
+
+        var Dano = CauDano(lvlpoke, atkpoke, poderpoke, mod);
+        if (crt)
+        {
+            Dano = Dano * 2;
+        }
+        Debug.Log("Dano= " + Dano);
+	}
+
+    int CauDano( int poder, int atk, int lvl, float modifi)
+    {
+        return ((((((2 * lvl) / 5) + 2) * poder * atk)/50)+2)*mod;
+        
+
+    }
+}
---
+++ Assets/idk.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class idk : MonoBehaviour {
+
+    public int n = 0;
+    int R;
+   	void Start ()
+    {
+        Weird(n);
+	}
+	
+    void Weird(int num=0)
+    {
+        R = n % 2;
+        if (R == 0)
+        {
+            if (n > 2 && n < 5)
+            {
+                Debug.Log("Não Estranho " + R);
+            }
+            else
+            {
+                if (n > 6 && n < 20)
+                {
+                    Debug.Log("Estranho " + R);
+                }
+                else
+                {
+                    if (n < 20)
+                    {
+                        Debug.Log("Não Estranho " + R);
+                    }
+                }
+
+            }
+        }
+        else
+        {
+            Debug.Log("Estranho " + R);
+        }
+    }
+}
